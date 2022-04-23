3f782c3c81bbdae3263c576e280470565fcca215
riderius <riderius.help@gmail.com>
2022-04-23 08:37:52 +0300

    Added installer for create symlinks to configs from $HOME
    Signed-off-by: riderius <riderius.help@gmail.com>

---
+++ install.sh
@@ -0,0 +1,39 @@
+PWD=$(pwd)
+HOME="$HOME/temp"
+
+# zsh configuration
+ZSH="$HOME/.oh-my-zsh/"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+rm $HOME/.zshrc
+ln -s $PWD/.zshrc $HOME/.zshrc
+ln -s $PWD/p10k.zsh $HOME/p10k.zsh
+
+# vim-like progs configuration
+ln -s $PWD/.vimrc $HOME/.vimrc
+mkdir -p $HOME/.config/nvim
+ln -s $PWD/.config/nvim/init.vim $HOME/nvim/init.vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# git configuration
+ln -s $PWD/.gitconfig $HOME/.gitconfig
+ln -s $PWD/.config/gitfiles $HOME/.config/gitfiles
+
+# emacs configuration
+mkdir $HOME/.emacs.d
+ln -s $PWD/.emacs.d/init.el $HOME/.emacs.d/init.el
+ln -s $PWD/.emacs.d/lisp $HOME/.emacs.d/lisp
+
+# ssh configuration
+ln -s $PWD/.ssh $HOME/.ssh
+
+# .local/bin configuration
+ln -s $PWD/.local/bin $HOME/.local/bin
+
+# i3 configuration
+ln -s $PWD/.config/i3 $HOME/.config/i3
+ln -s $PWD/.config/i3status $HOME/.config/i3status
+
+# fish configuration
+ln -s $PWD/.config/fish $HOME/.config/fish
