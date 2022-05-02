b2d5d1e9816573efd31e49bd644178954aa2056b
riderius <riderius.help@gmail.com>
2022-05-02 17:42:47 +0300

    Revert "emacs: added global-aggressive-indent-mode from https://github.com/Malabarba/aggressive-indent-mode"
    This reverts commit fea95bd9e937ecca1faf9a570e0b97c3753c2275.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -158,11 +158,11 @@ ispell-extra-args '("--sug-mode=ultra"))
 (setq package-selected-packages '(evil badwolf-theme elcord telephone-line magit flyspell lsp-mode yasnippet
                                   lsp-treemacs projectile hydra flycheck company avy which-key dap-mode lsp-ui
                                   all-the-icons cmake-mode evil-commentary undo-tree evil-terminal-cursor-changer
-                                  markdown-preview-mode magit-todos rainbow-delimiters aggressive-indent))
+                                  markdown-preview-mode magit-todos rainbow-delimiters))
 
 (when (cl-find-if-not #'package-installed-p package-selected-packages)
-    (package-refresh-contents)
-    (mapc #'package-install package-selected-packages))
+  (package-refresh-contents)
+  (mapc #'package-install package-selected-packages))
 
 (unless (display-graphic-p)
       (require 'evil-terminal-cursor-changer)
@@ -248,6 +248,3 @@ ispell-extra-args '("--sug-mode=ultra"))
 (require 'undo-hl) ; https://github.com/casouri/undo-hl
 (add-hook 'find-file-hook 'undo-hl-mode)
 (add-hook 'find-file-hook 'rainbow-delimiters-mode)
-
-;; https://github.com/Malabarba/aggressive-indent-mode
-(global-aggressive-indent-mode 1)
